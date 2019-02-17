59f4f3e5fa00d8942e6f78ade07c09e3bdd74b3d
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-02-17 15:47:00 +0900

    버그 수정
    일부 노치 디스플레이의 iOS 브라우저에서 가방 UI가 표시되지 않던 문제 수정.

--- js/inventory.js
+++ js/inventory.js
@@ -1,4 +1,12 @@
+function fixInventory() {
+  $('#inventory').style.top =
+    innerHeight * 0.9 + 'px';
+}
+
+window.addEventListener('resize', fixInventory);
+
 window.addEventListener('DOMContentLoaded', function () {
+  fixInventory();
   $('#inventory').addEventListener('click', function () {
     const isOpen = JSON.parse(this.getAttribute('open'));
     this.setAttribute('open', JSON.stringify(!isOpen));
